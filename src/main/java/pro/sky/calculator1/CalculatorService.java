08bdbaa2774493c002a66cdbe9d1d21b993703df
Evgeniy Malygin <black2001@inbox.ru>
2022-09-16 12:17:07 +0300

    Выполнено задание по теме Введение в Maven и Spring Boot

---
+++ src/main/java/pro/sky/calculator1/CalculatorService.java
@@ -0,0 +1,33 @@
+package pro.sky.calculator1;
+
+import org.springframework.stereotype.Service;
+@Service
+public class CalculatorService {
+
+    public String calculator() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String addition(double num1, double num2) {
+        double sum = num1 + num2;
+        return num1 + " + " + num2 + " = " + sum;
+    }
+
+    public String subtraction(double num1, double num2) {
+        double sum = num1 - num2;
+        return num1 + " - " + num2 + " = " + sum;
+    }
+
+    public String multiplication(double num1, double num2) {
+        double sum = num1 * num2;
+        return num1 + " * " + num2 + " = " + sum;
+    }
+    public String division(double num1, double num2) {
+        if (num2 ==0){
+            return "Деление на 0";
+        }
+        double sum = num1 / num2;
+        return num1 + " / " + num2 + " = " + sum;
+    }
+
+}
